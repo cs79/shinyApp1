09ddea40c1364b264d3944805d78de5120bda0a7
cs79 <cloudstrife79@gmail.com>
2014-06-21 22:38:28 -0400

    add financial ratio functions, flesh out UI a bit

--- server.R
+++ server.R
@@ -1,6 +1,17 @@
-### prediction functions go here
+## define financial ratio calculation functions
+#current ratio
+curRat <- function(curAssets, curLiab) {round(curLiab/curAssets*100, 0) }
+#quick ratio
+qRat <- function(curAssets, inv, curLiab) { round((curAssets-inv)/curLiab*100, 0) }
+#working capital ratio
+wcRat <- function(curAssets, curLiab) { round(curAssets/curLiab*100, 0) }
+#debt ratio
+debtRat <- function(liab, assets) { round(liab/assets*100, 0) }
 
+##define threshhold exceeded checker function
+threshCheck <- function() {}
 
+## shiny server
 shinyServer(
         function(input,output) {
                 
--- ui.R
+++ ui.R
@@ -2,11 +2,27 @@ shinyUI(pageWithSidebar(
                 headerPanel("Financial KPI Tracking Tool"),
         
                 sidebarPanel(
+                        #assets
+                        h3("Financial Data Entry"),
+                        h4("Assets"),
+                        numericInput("Cinput", "Cash on Hand", value=0),
+                        numericInput("ARinput", "Accounts Receivable", value=0),
+                        numericInput("Iinput", "Inventory", value=0)
+                        numericInput("OLAinput", "Other Liquid Assets", value=0),
+                        numericInput("LTAinput", "Long-term Assets", value=0),
+                        #liabilities
+                        h4("Liabilities"),
+                        numericInput("STLinput", "Short-term Liabilities", value=0),
+                        numericInput("LTLinput", "Long-term Liabilities", value=0),
+                        #kpi thresholds
+                        h3("Set KPI Thresholds (percent)"),
                         
                 ),
         
                 mainPanel(
-                        
+                        #explanation of how to use the tool
+                        #outputs from calculation
+                        #if i can manage it, a graph that changes colors under/over thresh
                 )
         )
 )
