ebaca5bd2629bc1d56a20a540fcedd74a3ce69be
cs79 <cloudstrife79@gmail.com>
2014-06-21 23:57:53 -0400

    complete basic app functionality

--- server.R
+++ server.R
@@ -1,19 +1,60 @@
 ## define financial ratio calculation functions
 #current ratio
-curRat <- function(curAssets, curLiab) {round(curLiab/curAssets*100, 0) }
+curRat <- function(curAssets, curLiab) {round(curLiab/curAssets, 2) }
 #quick ratio
-qRat <- function(curAssets, inv, curLiab) { round((curAssets-inv)/curLiab*100, 0) }
+qRat <- function(curAssets, inv, curLiab) { round((curAssets-inv)/curLiab, 2) }
 #working capital ratio
-wcRat <- function(curAssets, curLiab) { round(curAssets/curLiab*100, 0) }
+wcRat <- function(curAssets, curLiab) { round(curAssets/curLiab, 2) }
 #debt ratio
-debtRat <- function(liab, assets) { round(liab/assets*100, 0) }
+debtRat <- function(assets, liab) { round(liab/assets, 2) }
 
-##define threshhold exceeded checker function
-threshCheck <- function() {}
+##define threshhold checker functions
+aboveThreshCheck <- function(thresh, level) { if(thresh < level) {
+        return("WARNING: Threshold Exceeded")
+} else if(level/thresh > .85) {
+        return("NOTICE: Approaching Threshold")
+} else {
+        return("OK")
+}}
+
+belowThreshCheck <- function(thresh, level) { if(thresh > level) {
+        return("WARNING: Below Threshold")
+} else if(thresh/level > .85) {
+        return("NOTICE: Approaching Threshold")
+} else {
+        return("OK")
+}}
 
 ## shiny server
 shinyServer(
         function(input,output) {
-                
+                #calculate financial ratios from functions above
+                output$curRat <- renderPrint({curRat((input$Cinput + input$ARinput + 
+                                                              input$Iinput + input$OLAinput), 
+                                                     input$STLinput)})
+                output$qRat <- renderPrint({qRat((input$Cinput + input$ARinput + 
+                                                          input$OLAinput), input$Iinput, 
+                                                 input$STLinput)})
+                output$wcRat <- renderPrint({wcRat((input$Cinput + input$ARinput + 
+                                                            input$Iinput + input$OLAinput), 
+                                                   input$STLinput)})
+                output$debtRat <- renderPrint({debtRat((input$Cinput + input$ARinput + 
+                                                       input$Iinput + input$OLAinput + 
+                                                       input$LTAinput), 
+                                              (input$STLinput + input$LTLinput))})
+                #check if thresholds for ratios are being exceeded
+                output$curET <- renderPrint({belowThreshCheck(input$curThresh, curRat((input$Cinput + input$ARinput + 
+                                                                                   input$Iinput + input$OLAinput), 
+                                                                          input$STLinput))})
+                output$qET <- renderPrint({belowThreshCheck(input$qThresh, qRat((input$Cinput + input$ARinput + 
+                                                                             input$OLAinput), input$Iinput, 
+                                                                    input$STLinput))})
+                output$wcET <- renderPrint({belowThreshCheck(input$wcThresh, wcRat((input$Cinput + input$ARinput + 
+                                                                                input$Iinput + input$OLAinput), 
+                                                                       input$STLinput))})
+                output$debtET <- renderPrint({aboveThreshCheck(input$debtThresh, debtRat((input$Cinput + input$ARinput + 
+                                                                                      input$Iinput + input$OLAinput + 
+                                                                                      input$LTAinput), 
+                                                                             (input$STLinput + input$LTLinput)))})
         }
 )
--- ui.R
+++ ui.R
@@ -1,28 +1,56 @@
 shinyUI(pageWithSidebar(
                 headerPanel("Financial KPI Tracking Tool"),
-        
                 sidebarPanel(
                         #assets
                         h3("Financial Data Entry"),
                         h4("Assets"),
-                        numericInput("Cinput", "Cash on Hand", value=0),
-                        numericInput("ARinput", "Accounts Receivable", value=0),
-                        numericInput("Iinput", "Inventory", value=0)
-                        numericInput("OLAinput", "Other Liquid Assets", value=0),
-                        numericInput("LTAinput", "Long-term Assets", value=0),
+                        numericInput("Cinput", "Cash on Hand", value=100),
+                        numericInput("ARinput", "Accounts Receivable", value=100),
+                        numericInput("Iinput", "Inventory", value=100),
+                        numericInput("OLAinput", "Other Liquid Assets", value=100),
+                        numericInput("LTAinput", "Long-term Assets", value=100),
                         #liabilities
                         h4("Liabilities"),
-                        numericInput("STLinput", "Short-term Liabilities", value=0),
-                        numericInput("LTLinput", "Long-term Liabilities", value=0),
+                        numericInput("STLinput", "Short-term Liabilities", value=100),
+                        numericInput("LTLinput", "Long-term Liabilities", value=100),
                         #kpi thresholds
-                        h3("Set KPI Thresholds (percent)"),
-                        
+                        h3("Set KPI Alert Thresholds"),
+                        h4("(in percent)"),
+                        sliderInput("curThresh", "Current Ratio Threshold", value=0.9, min=0, max=5, step=0.1),
+                        sliderInput("qThresh", "Quick Ratio Threshold", value=0.9, min=0, max=5, step=0.1),
+                        sliderInput("wcThresh", "Working Capital Ratio Threshold", value=0.9, min=0, max=5, step=0.1),
+                        sliderInput("debtThresh", "Debt Ratio Threshold", value=0.6, min=0, max=1, step=0.1),
+                        width=5
                 ),
-        
                 mainPanel(
-                        #explanation of how to use the tool
-                        #outputs from calculation
-                        #if i can manage it, a graph that changes colors under/over thresh
+                        h3("How to use this tool:"),
+                        "In the sidebar, enter financial information about the assets and liabilities of your company.  
+                        (Some default values are provided, all of which can be adjusted by entering new numbers and/or changing the sliders).
+                        Then, select thresholds for financial ratios about which you would like to be alerted.  
+                        For Current Ratio, Quick Ratio, and Working Capital Ratio, the threshold specified will act as a maximum for warning.  
+                        For Debt Ratio, the threshold will act as a minimum for warning.
+                        The application will also give you a notice if you are approaching a threshold, 
+                        but have not yet crossed it.",
+                        h4("Current Ratio:"),
+                        verbatimTextOutput("curRat"),
+                        h4("Ratio Status:"),
+                        verbatimTextOutput("curET"),
+                        br(),
+                        h4("Quick Ratio:"),
+                        verbatimTextOutput("qRat"),
+                        h4("Ratio Status:"),
+                        verbatimTextOutput("qET"),
+                        br(),
+                        h4("Working Capital Ratio:"),
+                        verbatimTextOutput("wcRat"),
+                        h4("Ratio Status:"),
+                        verbatimTextOutput("wcET"),
+                        br(),
+                        h4("Debt Ratio:"),
+                        verbatimTextOutput("debtRat"),
+                        h4("Ratio Status:"),
+                        verbatimTextOutput("debtET"),
+                        width=7
                 )
         )
 )
